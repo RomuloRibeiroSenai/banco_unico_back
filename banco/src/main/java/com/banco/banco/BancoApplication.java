20e72ed15796a839356d26b1d5fa07fc5f90f659
RomuloRibeiroSenai <romulo.s.ribeiro@aluno.senai.br>
2023-11-12 16:34:06 -0300

    conexao com banco e classe operacao

--- banco/src/main/java/com/banco/banco/BancoApplication.java
+++ banco/src/main/java/com/banco/banco/BancoApplication.java
@@ -2,8 +2,10 @@ package com.banco.banco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.banco.entities")
 public class BancoApplication {
 
 	public static void main(String[] args) {
--- banco/src/main/java/com/banco/entities/Operacao.java
+++ banco/src/main/java/com/banco/entities/Operacao.java
@@ -1,7 +1,21 @@
 package com.banco.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+@Entity
 public class Operacao {
-    
-    // id;
-    // tipo;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String tipo;
+    private double valor;
+
+    @ManyToOne
+    private Cliente cliente;
 }
